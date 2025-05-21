16e71d7d3c586f8e58262654c30a77b1f3a128a2
MD Boni Amin <aminboni048@gmail.com>
2025-05-21 06:41:40 +0600

    2nd commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Page/Home.jsx'
+import ByCategory from './Page/ByCategory.jsx'
+import Details from './Page/Details.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+         <Route path='/' element={<Home/>}/>
+         <Route path='/ByCategory/:' element={<ByCategory/>}/>
+         <Route path='/Details/:id'  element={<Details/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
